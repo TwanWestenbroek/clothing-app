2d8ba27e7d6e826efd2d566d36148382b2cee94f
TwanWestenbroek <twan.westenbroek@gmail.com>
2021-02-24 13:23:57 +0100

    created cart-item component with reducer

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,12 +1,12 @@
 export const addItemToCart = (cartItems, cartItemToAdd) => {
-  const existingCartitem = cartItems.find(
-    (cartItem) => cartItemToAdd.id == cartItemToAdd.id
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToAdd.id
   );
 
-  if (existingCartitem) {
+  if (existingCartItem) {
     return cartItems.map((cartItem) =>
-      cartItem.id == cartItemToAdd.id
-        ? { ...cartItemToAdd, quantity: cartItem.quantity + 1 }
+      cartItem.id === cartItemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
         : cartItem
     );
   }
